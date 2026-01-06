a28a08820742d8f70828596d5cc1fd391e0b0ee6
Ben Thompson <124523309+Wally71551@users.noreply.github.com>
2026-01-06 22:21:00 +0000

    Basic setup for the project list

--- scripts/dataLoad.js
+++ scripts/dataLoad.js
@@ -1,28 +1,34 @@
-async function LoadMusicData() {
+async function LoadMusicData()
+{
     let json = await LoadFromJSON('data/music.json');
     return json.music;
 }
 
-async function LoadPlatformsData() {
+async function LoadPlatformsData()
+{
     let json = await LoadFromJSON('data/platforms.json');
     return json.platforms;
 }
 
-async function LoadProjectsData() {
+async function LoadProjectsData()
+{
     let json = await LoadFromJSON('data/projects.json');
     return json.projects;
 
 }
 
-async function LoadSeriesData() {
+async function LoadSeriesData()
+{
     let json = await LoadFromJSON('data/series.json');
     return json.series;
 }
 
-async function LoadFromJSON(jsonPath) {
+async function LoadFromJSON(jsonPath)
+{
     let response = await fetch(jsonPath);
 
-    if (!response.ok) {
+    if (!response.ok)
+    {
         throw new Error(`Failed to load ${path}: ${response.status}`);
     }
 
--- scripts/projectsLoad.js
+++ scripts/projectsLoad.js
@@ -3,12 +3,15 @@ let platforms;
 let series;
 
 const platformTemplate = document.getElementById("platforms-template");
+const projectTemplate = document.getElementById("project-grid");
 
 let platformHeaders = [];
+const platformProjects = new Map();
 
 Init();
 
-async function Init() {
+async function Init()
+{
     projects = await LoadProjectsData();
     platforms = await LoadPlatformsData();
     series = await LoadSeriesData();
@@ -16,29 +19,73 @@ async function Init() {
     PageLoad();
 }
 
-function PageLoad() {
-    //Load the platform headers first
+function PageLoad()
+{
+    //Set up the project setup arrays
+    projects.forEach(ProjectSetup);
+
+    //Sort each of the elements into release date order
+
     platforms.forEach(CreateHeader);
 }
 
-function CreateHeader(header, index) {
+function CreateHeader(header, index)
+{
     let headerElement = platformTemplate.content.cloneNode(true);
 
     let platformHeader = headerElement.querySelector(".platform-header");
     let headerTitle = headerElement.querySelector(".platform-title");
-    let contentElement = headerElement.querySelector(".platform-content");
+    let contentElement = headerElement.querySelector(".platform-content-grid");
 
     headerTitle.textContent = header.platformName;
-    //contentElement.innerHTML = content;
-    contentElement.hidden = true;
+    contentElement.hidden = false;
     platformHeader.addEventListener("click", () => {
         contentElement.hidden = !contentElement.hidden;
     });
 
+    let platProjects = platformProjects.get(header.platformID);
+    if(platProjects != undefined)
+    {
+        for(let i = 0; i < platProjects.length; i++)
+        {
+            let projectElement = projectTemplate.content.cloneNode(true);
+            let projectData = FindProject(platProjects[i]);
+
+            let image = projectElement.querySelector('img');
+            image.src = projectData.imageSrc;
+
+            contentElement.append(projectElement);
+        }
+    }
+
     document.documentElement.append(headerElement);
-    platformHeaders.push([headerElement, header.platformID])
+    platformHeaders.push([header.platformID, headerElement])
 }
 
-function ProjectSetup() {
+function ProjectSetup(project, index)
+{
+    for(let i = 0; i < project.platformIDs.length; i++)
+    {
+        if(platformProjects.has(project.platformIDs[i].platform))
+        {
+            let temp = platformProjects.get(project.platformIDs[i].platform);
+            temp.push(project.projectID);
+            platformProjects.set(project.platformIDs[i].platform, temp);
+        }
+        else
+        {
+            platformProjects.set(project.platformIDs[i].platform, [project.projectID]);
+        }
+    }
+}
 
+function FindProject(id)
+{
+    for(let i = 0; i < projects.length; i++)
+    {
+        if(projects[i].projectID == id)
+        {
+            return projects[i];
+        }
+    }
 }
