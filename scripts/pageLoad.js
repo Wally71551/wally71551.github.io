2e944ba66c42cc5c55a0459fdd079cbe7a9cb437
Ben Thompson <124523309+Wally71551@users.noreply.github.com>
2026-02-08 18:53:58 +0000

    Change to project page footer

--- scripts/pageLoad.js
+++ scripts/pageLoad.js
@@ -29,8 +29,9 @@ class ProjectFooter extends HTMLElement {
                     <li>Projects only contain the music that is on their original soundtrack (if it exists), otherwise it appears on the first project it appeared on.</li>
                     <ul>
                         <li>For sequels, if they are included on the original soundtrack it appears in that list too, if there is no soundtrack release it won't be there even if it's reused.</li>
+                        <li>This also applies to remixes, if a track is remixed in another project only the base version is used.</li>
                     </ul>
-                    <li>Enhanced versions / re-releases include both the base soundtrack plus new audio.</li>
+                    <li>Enhanced versions / re-releases include both the base soundtrack plus the new soundtrack/new tracks.</li>
                     <li>Remakes that use the same soundtrack also get the music listing on that project too.</li>
                 </ul>
             </div>
