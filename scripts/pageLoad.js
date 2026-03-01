657875337c802534b1c21062bcf2ba33226e8edc
Ben Thompson <124523309+Wally71551@users.noreply.github.com>
2026-03-01 01:24:05 +0000

    Data update

--- scripts/pageLoad.js
+++ scripts/pageLoad.js
@@ -51,6 +51,9 @@ class ProjectFooter extends HTMLElement {
                     <li>Enhanced versions / re-releases include both the base soundtrack plus the new soundtrack/new tracks.</li>
                     <li>Remakes that use the same soundtrack also get the music listing on that project too.</li>
                     <li>Ports are included unless they are in collections/compilations—larger-scope lines of emulated ports such as <em>Virtual Console</em>, <em>Nintendo Classics</em> or <em>PlayStation Plus Classics Catalogue</em> are also omitted as the games are presented completely as they were on the original platform.
+                    <ul>
+                        <li>However, this is not the case for projects that are first being made available through these avenues.
+                    </ul>
                     <li>All release dates are the UK dates where applicable, English language dates if no UK date exists, and then the initial release if none are beyond that.
                 </ul>
             </div>
