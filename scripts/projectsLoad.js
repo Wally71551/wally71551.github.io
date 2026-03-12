3c7c53aa833e149269589bf3fe20491ce9ad5ce0
Ben Thompson <124523309+Wally71551@users.noreply.github.com>
2026-03-12 22:48:19 +0000

    Track data popup

--- scripts/projectsLoad.js
+++ scripts/projectsLoad.js
@@ -187,7 +187,7 @@ function ShowProjectDetails(clickedElement, gridContainer)
 
         if(j == projectData.musicList.length - 1)
         {
-            trackDisplay.classList.add('track-list-item-end')
+            trackDisplay.classList.add('track-list-item-end');
         }
 
         trackList.appendChild(trackDisplay);
@@ -288,11 +288,13 @@ function SetUpTrackList(trackData, projectData)
         trackIconHeader.appendChild(MakeExternalLinkIcon("bandcamp", trackData.bandcampLink));
     }
 
-    panel.addEventListener('click', (e) => {
+    let textPanel = panel.querySelector('.track-list-header-text');
+    textPanel.addEventListener('click', (e) => {
             ShowTrackDetails(panel);
         });
 
     panel.dataset.trackId = trackData.musicID;
+    panel.dataset.projectID = projectData.projectID;
     return panel;
 }
 
@@ -301,6 +303,7 @@ let activeTrackList = "";
 function ShowTrackDetails(clickedElement)
 {
     let newTrackId = clickedElement.dataset.trackId;
+    let projectID = clickedElement.dataset.projectID;
 
     if(activeTrackDetailPanel && activeTrackList === newTrackId)
     {
@@ -319,15 +322,25 @@ function ShowTrackDetails(clickedElement)
     let trackData = GetTrack(newTrackId);
     let typeText = panel.querySelector('.track-type');
     typeText.textContent = trackData.musicType;
+
     let trackReleaseDate = panel.querySelector('.track-release-date');
-    trackReleaseDate.textContent = FormatDateToString(trackData.releaseDate);
+    trackReleaseDate.textContent = `Release Date: ${FormatDateToString(trackData.releaseDate)}`;
+
+    if(trackData.linkedAlbumID != projectID && trackData.linkedAlbumID && trackData.linkedAlbumID != "")
+    {
+        let album = panel.querySelector('.track-album');
+        album.innerHTML = `Project: ${FindTrackProjectName(trackData.linkedAlbumID)} #${trackData.linkedAlbumTrack}`;
+    }
+    else
+    {
+        panel.querySelector('.track-album');
+    }
+
     let trackArranger = panel.querySelector('.track-arranger');
     trackArranger.textContent = `Arranger: ${trackData.trackArranger}`
 
-    let trackComposers = panel.querySelector('.track-composer');
-    trackComposers.textContent = `Composer: ${trackData.composer}`;
-
     //Handling linked tracks
+    let trackLinksDisplaying = false;
     if(trackData.linkedTracks == null)
     {
         panel.querySelector('.linked-track-header').remove();
@@ -336,15 +349,16 @@ function ShowTrackDetails(clickedElement)
     {
         //Header setting
         let trackLinkedListHeader = panel.querySelector('.linked-track-header');
+        trackLinkDisplaying = true;
 
         switch(trackData.musicType)
         {
             case "Remix":
             case "Original":
-                trackLinkedListHeader.textContent = "Based On:"
+                trackLinkedListHeader.textContent = "Based On"
                 break;
             case "Medley":
-                trackLinkedListHeader.textContent = "Medley Tracks:"
+                trackLinkedListHeader.textContent = "Medley Tracks"
                 break;
         }
 
@@ -358,15 +372,61 @@ function ShowTrackDetails(clickedElement)
 
             let linkedTrackArtists = linkedTrackTemplate.querySelector('.linked-track-artists');
             linkedTrack.artists.forEach((artist) => {
+                let hasRef = artist.URL && artist.URL != ""
                 let artistTemplate = trackLinkedTrackArtistTemplate.content.cloneNode(true);
-                let artistElement = artistTemplate.querySelector('.track-linked-track-artistElement');
-                artistElement.textContent = `${artist.header} ${artist.artistName}`;
-                artistElement.href = artist.URL;
+                let artistElement = artistTemplate.querySelector('.linked-track-artist-info');
+
+                if(hasRef)
+                {
+                    artistElement.innerHTML = `<strong>${artist.header}:</strong> <a href="${artist.URL}">${artist.artistName}</a>`;
+                }
+                else
+                {
+                    artistElement.innerHTML = `<strong>${artist.header}:</strong> ${artist.artistName}`;
+                }
+                
                 linkedTrackArtists.appendChild(artistTemplate);
             });
 
+            //Remove bottom line
+            if(trackData.linkedTracks[trackData.linkedTracks.length - 1].name == linkedTrack.name)
+            {
+                
+                linkedTrackTemplate.querySelector('.track-linked-track-item').classList.add('track-linked-track-item-end');
+            }
             trackLinkedListDisplay.appendChild(linkedTrackTemplate);
         });
+    }
+
+    if(trackData.composer && trackData.composer != "")
+    {
+        let trackComposers = panel.querySelector('.track-composer');
+        trackComposers.innerHTML = `<strong>Composer:</strong> ${trackData.composer}`;
+    }
+    else
+    {
+        panel.querySelector('.track-composer').remove();
+    }
+
+    //Handle original track artists
+    if(trackData.artists && trackData.artists.length > 0)
+    {
+        let trackArtists = panel.querySelector('.track-info-list');
+        trackData.artists.forEach((artistData) => {
+            let artistTemplate = trackInfoListTemplate.content.cloneNode(true);
+            let artistText = artistTemplate.querySelector('.artist-info');
+            artistText.innerHTML = `<strong>${artistData.header}:</strong> ${artistData.artistName}`;
+            trackArtists.appendChild(artistTemplate);
+        })
+    }
+    else
+    {
+        panel.querySelector('.track-info-list').remove();
+    }
+
+    if(trackLinkDisplaying || !trackData.artists || trackData.artists.length <= 0)
+    {
+        panel.querySelector('.track-info-header').remove();
     }
 
     //Handling transcribers
@@ -379,28 +439,11 @@ function ShowTrackDetails(clickedElement)
         let trackTranscriberListDisplay = panel.querySelector('.track-transcriber-list');
         trackData.transcription.forEach((transcriptionData) => {
             let transcriptionTemplate = trackTranscriberListTemplate.content.cloneNode(true);
-            let link = transcriptionTemplate.querySelector('.transcriber-info');
+            let link = transcriptionTemplate.querySelector('.artist-info');
             link.textContent = transcriptionData.transcriber;
             link.href = transcriptionData.url;
             trackTranscriberListDisplay.appendChild(transcriptionTemplate);
         });
-    }
-
-    //Handle original track artists
-    if(trackData.artists && trackData.artists.length > 0)
-    {
-        let trackArtists = panel.querySelector('.track-info-list');
-        trackData.artists.forEach((artistData) => {
-            let artistTemplate = trackInfoListTemplate.content.cloneNode(true);
-            let artistText = artistTemplate.querySelector('.transcriber-info');
-            artistText.textContent = `${artistData.header}: ${artistData.artistName}`;
-            trackArtists.appendChild(artistTemplate);
-        })
-    }
-    else
-    {
-        panel.querySelector('.track-info-header').remove();
-        panel.querySelector('.track-info-list').remove();
     }
 
     //Handling description
