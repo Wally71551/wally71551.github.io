896a4e9800d6894d8da9beb75a653cb2849cd1bd
Ben Thompson <124523309+Wally71551@users.noreply.github.com>
2026-06-21 01:06:31 +0100

    Timeline logic - site v0.2

---
+++ scripts/timelineLoad.js
@@ -0,0 +1,72 @@
+let timeline;
+let currentTimelineYear;
+let currentTimelineSection;
+let currentTimelineGrid;
+let timelineContainer;
+
+const timelineYearTemplate = document.getElementById("timeline-year-template");
+const timelineDateTemplate = document.getElementById("timeline-date-template");
+const timelineEntryTemplate = document.getElementById("timeline-entry-template");
+
+Init();
+
+async function Init()
+{
+    timeline = await LoadTimelineData();
+    currentTimelineYear = -1;
+
+    PageLoad();
+}
+
+function PageLoad()
+{
+    timelineContainer = document.getElementsByClassName("timeline-container")[0];
+    timeline.forEach(AddEntry);
+}
+
+function AddEntry(entry, index)
+{
+    //Check for new timeline year
+    let date = entry.entryDate.split('-');
+    let year = date[0];
+    if(year > currentTimelineYear)
+    {
+        currentTimelineSection = CreateNewSection(year);
+    }
+
+    let timelineDateObject = timelineDateTemplate.content.cloneNode(true);
+    let timelineDateDisplay = timelineDateObject.querySelector('.timeline-date');
+    timelineDateDisplay.innerText = date[2] + "/" + date[1];
+
+    let timelineEntryObject = timelineEntryTemplate.content.cloneNode(true);
+    let timelineEntryDisplay = timelineEntryObject.querySelector('.timeline-entry-text');
+    timelineEntryDisplay.innerHTML = entry.entryText;
+
+    if(entry.entryURL != null && entry.entryURL != "")
+    {
+        let timelineLinkDisplay = timelineEntryObject.querySelector('.timeline-entry-link');
+        timelineLinkDisplay.href = entry.entryURL;
+        let timelineLinkImage = timelineLinkDisplay.querySelector('.timeline-link-image');
+        timelineLinkImage.src = "icons/" + entry.entryURLImage + ".svg";
+    }
+    else
+    {
+        timelineEntryObject.querySelector('.timeline-entry-link').remove();
+    }
+
+    currentTimelineGrid.insertBefore(timelineEntryObject, currentTimelineGrid.firstChild);
+    currentTimelineGrid.insertBefore(timelineDateObject, currentTimelineGrid.firstChild);
+}
+
+function CreateNewSection(year)
+{
+    currentTimelineYear = year;
+    let newTimelineSection = timelineYearTemplate.content.cloneNode(true);
+    currentTimelineSection = newTimelineSection.object;
+    timelineYearHeader = newTimelineSection.querySelector('.timeline-year-heading');
+    timelineYearHeader.innerText = year;
+    currentTimelineGrid = newTimelineSection.querySelector('.timeline-grid');
+
+    timelineContainer.insertBefore(newTimelineSection, timelineContainer.firstChild);
+    return newTimelineSection;
+}
